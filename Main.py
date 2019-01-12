507aac13145db44c69e2b315cdc01665b6e94cc6
RapidFires <46613723+RapidFires@users.noreply.github.com>
2019-01-11 16:13:36 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,3 +5,12 @@ bot = commands.Bot(command_prefix = ":")
 
 @bot.event
 async def on_ready():
+  print("Logged in as")
+  print("User name:", bot.user.name)
+  print("User ID:", bot.user.id)
+  
+@bot.command()
+async def ping():
+  await bot.say("pong!")
+  
+bot.run(os.environ['BOT_TOKEN'])
