7b1b75cd85aa661e8d1e8ceddfb7fbb265b7711d
RapidFires <46613723+RapidFires@users.noreply.github.com>
2019-01-29 23:19:53 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,13 +1,27 @@
 import discord
 from discord.ext import commands
 import asyncio
+import requests, bs4
 import os
+import time
+import youtube_dl
 import inspect
 import datetime
 from discord import opus
 
-bot = commands.Bot(command_prefix = "+")
+start_time = time.time()
+
+bot = commands.Bot(command_prefix=("m."))
 bot.remove_command("help")
+
+players = {}
+queues = {}
+
+def check_queue(id):
+	if queues[id] != []:
+		player = queues[id].pop(0)
+		players[id] = player
+		player.start()
 
 @bot.event 
 async def on_ready():
@@ -16,12 +30,109 @@ async def on_ready():
 	print("User id:", bot.user.id)
 	print('---------------')
 
+@bot.command(pass_context=True, no_pm=True)
+async def ping(ctx):
+    pingtime = time.time()
+    pingms = await client.say("Pinging...")
+    ping = (time.time() - pingtime) * 1000
+    await bot.edit_message(pingms, "Pong! :ping_pong: ping time is `%dms`" % ping)
+    
+@bot.command(name="join", pass_context=True, no_pm=True)
+async def _join(ctx):
+    user = ctx.message.author
+    channel = ctx.message.author.voice.voice_channel
+    await bot.join_voice_channel(channel)
+    embed = discord.Embed(colour=user.colour)
+    embed.add_field(name="Successfully connected to voice channel:", value=channel)
+    await bot.say(embed=embed)
+	
+@bot.command(name="leave", pass_context=True, no_pm=True)
+async def _leave(ctx):
+    user = ctx.message.author
+    server = ctx.message.server
+    channel = ctx.message.author.voice.voice_channel
+    voice_client = bot.voice_client_in(server)
+    await voice_client.disconnect()
+    embed = discord.Embed(colour=user.colour)
+    embed.add_field(name="Successfully disconnected from:", value=channel)
+    await bot.say(embed=embed)
+
+@bot.command(pass_context=True)
+async def pause(ctx):
+    user = ctx.message.author
+    id = ctx.message.server.id
+    players[id].pause()
+    embed = discord.Embed(colour=user.colour)
+    embed.add_field(name="Player Paused", value=f"Requested by {ctx.message.author.name}")
+    await bot.say(embed=embed)
+
+@bot.command(pass_context=True)
+async def skip(ctx):
+    user = ctx.message.author
+    id = ctx.message.server.id
+    players[id].stop()
+    embed = discord.Embed(colour=user.colour)
+    embed.add_field(name="Player Skipped", value=f"Requested by {ctx.message.author.name}")
+    await bot.say(embed=embed)
+	
+@bot.command(name="play", pass_context=True)
+async def _play(ctx, *, name):
+	author = ctx.message.author
+	name = ctx.message.content.replace("m.play ", '')
+	fullcontent = ('http://www.youtube.com/results?search_query=' + name)
+	text = requests.get(fullcontent).text
+	soup = bs4.BeautifulSoup(text, 'html.parser')
+	img = soup.find_all('img')
+	div = [ d for d in soup.find_all('div') if d.has_attr('class') and 'yt-lockup-dismissable' in d['class']]
+	a = [ x for x in div[0].find_all('a') if x.has_attr('title') ]
+	title = (a[0]['title'])
+	a0 = [ x for x in div[0].find_all('a') if x.has_attr('title') ][0]
+	url = ('http://www.youtube.com'+a0['href'])
+	server = ctx.message.server
+	voice_client = bot.voice_client_in(server)
+	player = await voice_client.create_ytdl_player(url, after=lambda: check_queue(server.id))
+	players[server.id] = player
+	print("User: {} From Server: {} is playing {}".format(author, server, title))
+	player.start()
+	embed = discord.Embed(description=" ")
+	embed.add_field(name="Now Playing", value=title)
+	await bot.say(embed=embed)
+	
+@bot.command(pass_context=True)
+async def queue(ctx, *, name):
+	name = ctx.message.content.replace("+queue ", '')
+	fullcontent = ('http://www.youtube.com/results?search_query=' + name)
+	text = requests.get(fullcontent).text
+	soup = bs4.BeautifulSoup(text, 'html.parser')
+	img = soup.find_all('img')
+	div = [ d for d in soup.find_all('div') if d.has_attr('class') and 'yt-lockup-dismissable' in d['class']]
+	a = [ x for x in div[0].find_all('a') if x.has_attr('title') ]
+	title = (a[0]['title'])
+	a0 = [ x for x in div[0].find_all('a') if x.has_attr('title') ][0]
+	url = ('http://www.youtube.com'+a0['href'])
+	server = ctx.message.server
+	voice_client = bot.voice_client_in(server)
+	player = await voice_client.create_ytdl_player(url, after=lambda: check_queue(server.id))
+	
+	if server.id in queues:
+		queues[server.id].append(player)
+	else:
+		queues[server.id] = [player]
+	embed = discord.Embed(description=" ")
+	embed.add_field(name="Video queued", value=title)
+	await bot.say(embed=embed)
+
+@bot.command(pass_context=True)
+async def resume(ctx):
+    user = ctx.message.author
+    id = ctx.message.server.id
+    players[id].resume()
+    embed = discord.Embed(colour=user.colour)
+    embed.add_field(name="Player Resumed", value=f"Requested by {ctx.message.author.name}")
+    await bot.say(embed=embed)
+
 def user_is_me(ctx):
 	return ctx.message.author.id == "474257464368431144","277983178914922497"
-  
-@bot.command()
-async def ping():
-	await bot.say("pong!")
   
 @bot.command(name="clean", pass_context=True, no_pm=True)
 @commands.has_permissions(administrator=True)
@@ -150,8 +261,8 @@ async def help(ctx):
 	embed = discord.Embed(title="Help section", description=" ", color=0xFFFF)
 	embed.add_field(name="+join", value="make the bot join voice channel")
 	embed.add_field(name="+leave", value="make the bot leave the voice channel")
-	embed.add_field(name="+play", value="please be careful when using this command it will break if theres music playing.")
-	embed.add_field(name="+stop", value="to stop the music from playing")
+	embed.add_field(name="+play", value="please be careful when using this command it will break if theres music playing. if you want to add music use +queue (song name) without ( )")
+	embed.add_field(name="+skip", value="to skip the music")
 	embed.add_field(name="+warn", value="+warn @user <reason>")
 	embed.add_field(name="+mute", value="+mute @user <reason>")
 	embed.add_field(name="+unmute", value="+unmute @user <reason>")
